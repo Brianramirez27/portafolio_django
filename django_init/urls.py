006962e65d1f63fb687dad13d42968973156cc9d
Brayan Ramirez <brayanramirez4546@gmail.com>
2022-04-14 13:37:22 -0500

    prueba de static

--- django_init/urls.py
+++ django_init/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('first_app.urls')), # se enlasa los urls de la aplicacion  alas urls globales
+   
 ]
